adfab67302374daa22f11bf095732ea179529961
Elias Batista <eliasbatistasouza@gmail.com>
2023-08-22 11:04:22 -0300

    Call create_map from Home

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
-from folium.plugins import MarkerCluster
 
 from utils import general_data
 from utils import home_data
@@ -48,35 +45,7 @@ def main():
     st.markdown('---')
     
     with st.container():
-        m = folium.Map()
-        marker_cluster = MarkerCluster().add_to(m)
-    
-        for _, line in df.iterrows():
-    
-            name = line['restaurant_name']
-            price_for_two = line['average_cost_for_two']
-            cuisine = line['cuisines']
-            rating = line['aggregate_rating']
-            color = f"{line['color_name']}"
-    
-            html = '<p><strong>{}</strong></p>'
-            html += '<p>Price: {:.} ($) for two'.replace(".", ",")
-            html += '<br/>Type: {}'
-            html += '<br/>Rating: {}/5.0'
-            html = html.format(name, price_for_two, cuisine, rating)
-    
-            popup = folium.Popup(
-                folium.Html(html, script=True),
-                max_width=500,
-            )
-    
-            folium.Marker(
-                [line['latitude'], line['longitude']],
-                popup=popup,
-                icon=folium.Icon(color=color, icon='home', prefix='fa'),
-            ).add_to(marker_cluster)
-    
-        folium_static(m, width=1024, height=600)
+        home_data.create_map(df)
     return None
 
 if __name__ == '__main__':
